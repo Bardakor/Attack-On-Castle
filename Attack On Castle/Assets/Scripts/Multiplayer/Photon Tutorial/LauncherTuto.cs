6277fd6247bdf8bd15dd4cf8ea2cfdb4c712eb4c
Bardakor <liam-alexandre.abourousse@epita.fr>
2021-12-21 02:52:36 +0100

    Avancements majeurs dans le multi
    - Créations de salles d'attentes variant selon le nombre de joueurs
    - Major glitch fixes

--- Attack On Castle/Assets/Scripts/Multiplayer/Photon Tutorial/LauncherTuto.cs
+++ Attack On Castle/Assets/Scripts/Multiplayer/Photon Tutorial/LauncherTuto.cs
@@ -21,15 +21,21 @@ public class LauncherTuto : MonoBehaviourPunCallbacks
     /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
     string gameVersion = "1";
 
+    /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+    /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+    /// Typically this is used for the OnConnectedToMaster() callback.
+    bool isConnecting;
+
+
     #endregion
-    
+
     #region Public Fields
-    
+
     [Tooltip("The Ui Panel to let the user enter name, connect and play")]
 
     [SerializeField]
     private GameObject controlPanel;
-    
+
     [Tooltip("The UI Label to inform the user that the connection is in progress")]
     [SerializeField]
     private GameObject progressLabel;
@@ -70,8 +76,8 @@ public class LauncherTuto : MonoBehaviourPunCallbacks
         }
         else
         {
-            // #Critical, we must first and foremost connect to Photon Online Server.
-            PhotonNetwork.ConnectUsingSettings();
+            // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = gameVersion;
         }
 
@@ -85,8 +91,12 @@ public class LauncherTuto : MonoBehaviourPunCallbacks
     {
         Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
 
-        //#Critical : The first we try do is to join a potential existin room. If there is, good, else, we'll be called back with OnJoinfRandomFailed()
-        PhotonNetwork.JoinRandomRoom();
+        if (isConnecting)
+        {
+            //#Critical : The first we try do is to join a potential existin room. If there is, good, else, we'll be called back with OnJoinfRandomFailed()
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
@@ -94,7 +104,7 @@ public class LauncherTuto : MonoBehaviourPunCallbacks
         progressLabel.SetActive(false);
         controlPanel.SetActive(true);
         Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
-
+        isConnecting = false;
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
@@ -108,6 +118,15 @@ public class LauncherTuto : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the 'Room for 1' ");
+
+            // #Critical: Load the Room Level.
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
     }
 
     #endregion
