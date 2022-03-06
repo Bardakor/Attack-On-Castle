4cb4de9bc3d5bca2a84996dcd653cbcbdc2c527b
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-03-06 16:14:33 +0100

    Assets + beginning of shop
    assets arbalete + chevalier et debut du shop avec le cannonitem sur CameraScene

---
+++ Attack On Castle/Assets/Scripts/ShopMenu/Shop.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseCannonItem()
+    {
+        Debug.Log("Cannon Item Purchased");
+    }
+    
+}
