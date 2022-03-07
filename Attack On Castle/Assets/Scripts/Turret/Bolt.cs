b67537a5ee5aa3df9f3faf8a222f51cc79d57ddb
Aurelien Daudin <aurelien.daudin@epita.fr>
2022-03-07 13:52:08 +0100

    Ballista lock enemies + fire bolts
    Implementing Turret script to lock at enemy and a bolt script to manage the arrow from the ballista to the ennemy. Added an effect when you hit ennemy.
    ---On CameraGameScene---

---
+++ Attack On Castle/Assets/Scripts/Turret/Bolt.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 70f;
+    public GameObject impactEffect;
+    //Find the target of the turret
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+        {
+            GameObject effectIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
+            Destroy (effectIns, 2f);
+
+            //Destroy(target.gameObject);
+            Destroy(gameObject);
+        }
+    
+}
