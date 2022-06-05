82818c6086cec7d755921b5fad894782548feb6c
Liam Abourousse <75449798+Bardakor@users.noreply.github.com>
2022-06-05 21:18:51 +0200

    Changed PlayerPrefs

--- Attack On Castle/Assets/Scripts/Multiplayer/UpdatedMulti/UMPlayerNameManager.cs
+++ Attack On Castle/Assets/Scripts/Multiplayer/UpdatedMulti/UMPlayerNameManager.cs
@@ -9,8 +9,26 @@ public class UMPlayerNameManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField usernameInput;
     
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("Username"))
+        {
+            usernameInput.text = PlayerPrefs.GetString("Username");
+            PhotonNetwork.NickName = PlayerPrefs.GetString("Username");        
+        }
+
+        else
+        {
+            usernameInput.text = "Player" + Random.Range(0, 10000).ToString("0000");
+            OnUsernameInputValueChanged();
+        }
+    }
+    
+    
+    
     public void OnUsernameInputValueChanged()
     {
         PhotonNetwork.NickName = usernameInput.text;
+        PlayerPrefs.SetString("username", usernameInput.text);
     }
 }
